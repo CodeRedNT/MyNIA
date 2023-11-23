83f1eda6f666d9247b462cf213d39f20aed317aa
Ranyele Amorim <rcode2001@gmail.com>
2023-11-22 22:16:11 -0300

    Initial commit

---
+++ app/src/main/java/com/coderednt/mynia/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.coderednt.mynia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        repeat(3) {
+            GlobalScope.launch {
+                Log.d("MyNIA", "Thread processada ${Thread.currentThread()}")
+            }
+        }
+    }
+}
